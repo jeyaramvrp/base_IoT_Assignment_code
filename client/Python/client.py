c535d949909d38a54cbec8334501e949036a1747
Jeyaraman <rjayavrp2@gmail.com>
2018-12-11 15:26:26 +0000

    Update python script for plotting data from HTTP response

--- client/Python/client.py
+++ client/Python/client.py
@@ -1,8 +1,25 @@
 #Base Python client for MEng in IoT Assignment
 #consumes data from IoT Gateway
 import urllib2
+import matplotlib.pyplot as tempgraph
+
+#For plotting
+tempgraph.ion()
+fig=tempgraph.figure()
+tempgraph.axis([0,12,0,99])
 
 response = urllib2.urlopen('http://localhost:8080/')
 resp = response.read()
 
-print resp
+#Read Response
+resp = resp.split('\n')
+#Time in X axis and temp on Y axis
+for data in resp:
+  try:
+      x=int(data[15:17])
+      y=int(data[40:42])
+      tempgraph.scatter(x,y);
+      tempgraph.show()
+      tempgraph.pause(0.001)
+  except:
+      pass
